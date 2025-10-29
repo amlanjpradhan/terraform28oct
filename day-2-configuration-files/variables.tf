a0db68427429d257a9d3596edff31538726c9b24
amlanjpradhan <amlanjyoti.pradhan@gmail.com>
2025-10-29 07:53:38 +0530

    push code

---
+++ day-2-configuration-files/variables.tf
@@ -0,0 +1,12 @@
+variable "ami_id" {
+    description="Passing the ami value"
+    default = ""
+    type = string
+  
+}
+variable "type" {
+    description = "passing value to instance type"
+    default = ""
+    type = string
+  
+}
